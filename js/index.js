5aa13c6c7ec03dab397e21cf0987f11d76def29b
UchVuch <85136486+UchVuch@users.noreply.github.com>
2023-02-25 20:29:05 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,51 @@
-const swiper = new Swiper(".mySwiper", {
-  direction: 'vertical',
-  slidesPerView: 20,
-  slidesPerGroup: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.portfolio__slider-next',
-    prevEl: '.portfolio__slider-prev',
-  },
-});
+function startApp() {
+  const swiper = new Swiper(".mySwiper", {
+    direction: 'vertical',
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.portfolio__slider-next',
+      prevEl: '.portfolio__slider-prev',
+    },
+
+    breakpoints: {
+      541: {
+        slidesPerView: 9,
+        slidesPerGroup: 9,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      },
+      768: {
+        slidesPerView: 12,
+        slidesPerGroup: 4,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      },
+      1001: {
+        slidesPerView: 20,
+        slidesPerGroup: 20,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      },
+    }
+  })
+
+  const buttonOpenMenu = document.querySelector(`[data-menu="open"]`)
+  const buttonCloseMenu = document.querySelector(`[data-menu="close"]`)
+  const navMenu = document.querySelector(`[data-menu="menu"]`)
+
+  const openMenu = () => navMenu.classList.add('header__nav--active')
+  const closeMenu = () => navMenu.classList.remove('header__nav--active')
+
+  buttonOpenMenu.addEventListener('click', openMenu)
+  buttonCloseMenu.addEventListener('click', closeMenu)
+}
+
+window.addEventListener('DOMContentLoaded', startApp)
