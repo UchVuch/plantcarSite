4054bbc08299dafed5c977bce39d0225923c7fd8
UchVuch <85136486+UchVuch@users.noreply.github.com>
2023-02-25 13:08:50 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
-const mainSwiper = new Swiper('.main-swiper', {
+const swiper = new Swiper(".mySwiper", {
   direction: 'vertical',
-  slidesPerView: 1,
-  //     grid: {
-  //       rows: 2,
-  //     },
-  spaceBetween: 10,
-
+  slidesPerView: 20,
+  slidesPerGroup: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: '.portfolio__slider-next',
     prevEl: '.portfolio__slider-prev',
   },
-  
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-})
+});
